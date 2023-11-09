b367d9d0ffb0e962c2d48612e50633076567eaca
Marcos <marcos-souza@outlook.com>
2023-11-09 11:07:58 -0300

    Correções de dados sensíveis

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,12 +6,12 @@ terraform {
         }
     }
 }
-provider "aws" {
-    region                    = var.region
-    shared_config_files       = [var.shared_config_files]
-    shared_credentials_files  = [var.shared_credentials_files]
-    profile                   = var.credential_profile
-    default_tags {
-		tags = var.default_tags
-	}
-}
+#provider "aws" {
+    #region                    = var.region
+    #shared_config_files       = [var.shared_config_files]
+    #shared_credentials_files  = [var.shared_credentials_files]
+    #profile                   = var.credential_profile
+    #default_tags {
+		#tags = var.default_tags
+	#}
+#}
