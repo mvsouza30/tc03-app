716224a975e5aa4ab3571ed264c400885628b88c
Marcos <marcos-souza@outlook.com>
2023-11-09 09:01:57 -0300

    Correções de dados sensíveis

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,12 +3,10 @@ terraform {
     required_providers {
         aws = {
             source  = "hashicorp/aws"
-            version = "~> 5.1.0"
+            version = "~> 3.0"
         }
     }
 }
 provider "aws" {
     region      = var.region
-    access_key  = var.access_key_id
-    secret_key  = var.secret_key
 }
