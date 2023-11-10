1bcbcaf113d4062589573f9c25afff124753aebb
Marcos <marcos-souza@outlook.com>
2023-11-10 08:29:54 -0300

    Rollback de configurações do ECS e redes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,90 +1,95 @@
-###################################################################################
-# This file describes the vpc, internet gateway, nat gateway and route tables
-###################################################################################
-
-resource "aws_vpc" "vpc" {
-    cidr_block              = var.vpc_cidr
-    enable_dns_hostnames    = true
-    enable_dns_support      = true
+resource "aws_vpc" "mainvpc"{
+    cidr_block           = "172.31.0.0/16"
+    instance_tenancy     = "default"
+    enable_dns_hostnames = true
 }
 
-resource "aws_internet_gateway" "igw" {
-    vpc_id                  = aws_vpc.vpc.id
-}
-
-resource "aws_nat_gateway" "ngw" {
-    allocation_id           = aws_eip.nateip.id
-    subnet_id               = aws_subnet.public_subnet_1.id
-    depends_on              = [ aws_internet_gateway.igw ]
-}
-
-resource "aws_eip" "nateip" {
-}
-
-resource "aws_subnet" "private_subnet_1" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = var.private_subnet_1
-    availability_zone       = var.availibilty_zone_1
-}
-
-resource "aws_subnet" "private_subnet_2" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = var.private_subnet_2
-    availability_zone       = var.availibilty_zone_2
-}
-
-resource "aws_subnet" "public_subnet_1" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = var.public_subnet_1
-    availability_zone       = var.availibilty_zone_1
+resource "aws_subnet" "sn1"{
+    cidr_block              = "172.31.1.0/24"
+    vpc_id                  = aws_vpc.mainvpc.id
+    availability_zone       = "us-east-1a"
     map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "public_subnet_2" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = var.public_subnet_2
-    availability_zone       = var.availibilty_zone_2
+resource "aws_subnet" "sn2"{
+    cidr_block              = "172.31.2.0/24"
+    vpc_id                  = aws_vpc.mainvpc.id
+    availability_zone       = "us-east-1b"
     map_public_ip_on_launch = true
 }
 
-resource "aws_route_table" "public" {
-    vpc_id                  = aws_vpc.vpc.id
-    
+resource "aws_subnet" "sn3"{
+    cidr_block              = "172.31.3.0/24"
+    vpc_id                  = aws_vpc.mainvpc.id
+    availability_zone       = "us-east-1c"
+    map_public_ip_on_launch = true
 }
 
-resource "aws_route" "public" {
-    route_table_id          = aws_route_table.public.id
-    destination_cidr_block  = "0.0.0.0/0"
-    gateway_id              = aws_internet_gateway.igw.id
+resource "aws_security_group" "sg"{
+    name    = "sg"
+    vpc_id  = aws_vpc.mainvpc.id
+
+    ingress{
+        description = "https"
+        from_port   = 5000
+        to_port     = 5000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress{
+        description = "https"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress{
+        description = "https"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress{
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
-resource "aws_route_table_association" "public1" {
-    subnet_id               = aws_subnet.public_subnet_1.id
-    route_table_id          = aws_route_table.public.id
-}
-resource "aws_route_table_association" "public2" {
-    subnet_id               = aws_subnet.public_subnet_2.id
-    route_table_id          = aws_route_table.public.id
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.mainvpc.id
 }
 
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.mainvpc.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
 
-resource "aws_route_table" "private" {
-    vpc_id                  = aws_vpc.vpc.id
-}
-  
-resource "aws_route" "private" {
-    route_table_id          = aws_route_table.private.id
-    destination_cidr_block  = "0.0.0.0/0"
-    nat_gateway_id          = aws_nat_gateway.ngw.id
-}
-
-resource "aws_route_table_association" "private1" {
-    subnet_id               = aws_subnet.private_subnet_1.id
-    route_table_id          = aws_route_table.private.id
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
 }
 
-resource "aws_route_table_association" "private2" {
-    subnet_id               = aws_subnet.private_subnet_2.id
-    route_table_id          = aws_route_table.private.id
+resource "aws_route_table_association" "route1" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id      = aws_subnet.sn1.id
+}
+
+resource "aws_route_table_association" "route2" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id      = aws_subnet.sn2.id
+}
+
+resource "aws_route_table_association" "route3" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id      = aws_subnet.sn3.id
 }
