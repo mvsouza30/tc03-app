0bc9b50b01653e19e564a6d65b4d296816696c96
Marcos <marcos-souza@outlook.com>
2023-11-10 09:45:15 -0300

    Rollback de configurações do ECS e redes

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,7 +3,7 @@
 ########################################################################################
 
 resource "aws_iam_role" "ecsTaskExecutionRole" {
-    name                  = "app-ecsTaskExecutionRole"
+    name                  = "ecsTaskExecutionRole"
     assume_role_policy    = data.aws_iam_policy_document.assume_role_policy.json
 }
 
