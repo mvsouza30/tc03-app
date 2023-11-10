eaa3f71f63edfd6f9b9192243826414c2f18dcb3
Marcos <marcos-souza@outlook.com>
2023-11-10 00:41:20 -0300

    Correções de dados sensíveis

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,7 +11,7 @@ terraform {
 provider "aws" {
     region                    = var.region
     #shared_config_files       = [var.shared_config_files]
-    #shared_credentials_files  = [var.shared_credentials_files]
+    shared_credentials_files  = ["/home/mvsouza/.aws/credentials"]
     profile                   = var.credential_profile
     default_tags {
 		tags = var.default_tags
