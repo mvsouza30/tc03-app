9c8c708dba5f3f81fcf4a93c450b843f4bdd362f
Marcos <marcos-souza@outlook.com>
2023-11-10 07:37:48 -0300

    Aumento das capacidades das tasks para testar o health

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -38,8 +38,8 @@ resource "aws_ecs_task_definition" "task_definition" {
     # Fargate
     requires_compatibilities            = ["FARGATE"]
     network_mode                        = "awsvpc"
-    cpu                                 = "256"
-    memory                              = "512"
+    cpu                                 = "512"
+    memory                              = "1024"
     execution_role_arn                  = aws_iam_role.ecsTaskExecutionRole.arn
     task_role_arn                       = aws_iam_role.ecsTaskRole.arn
 }
