329a597401f877d0370b00372ffa1f8b56ba14dd
Marcos <marcos-souza@outlook.com>
2023-11-10 02:25:34 -0300

    Correções de dados sensíveis

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,7 @@
+terraform {
+    required_version = ">= 0.12"
+}
+
 provider "aws" {
-    region      = var.region
-    version     = "~> 2.0"
-    
-    #shared_config_files       = ["/home/mvsouza/.aws/config"]
-    #shared_credentials_files  = ["/home/mvsouza/.aws/credentials"]
-    #profile                   = "default"
-    #default_tags {
-		#tags = var.default_tags
-	#}
+    region = var.region
 }
