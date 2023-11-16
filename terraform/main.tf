9715874f200f06440553a1f9ad9cb0f6a2bf39ab
Marcos <marcos-souza@outlook.com>
2023-11-16 20:15:19 -0300

    Alteração da classe de IPs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,25 @@
 resource "aws_vpc" "mainvpc"{
-    cidr_block           = "172.31.0.0/16"
+    cidr_block           = "10.0.0.0/16"
     instance_tenancy     = "default"
     enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "sn1"{
-    cidr_block              = "172.31.1.0/24"
+    cidr_block              = "10.0.1.0/24"
     vpc_id                  = aws_vpc.mainvpc.id
     availability_zone       = "us-east-1a"
     map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "sn2"{
-    cidr_block              = "172.31.2.0/24"
+    cidr_block              = "10.0.2.0/24"
     vpc_id                  = aws_vpc.mainvpc.id
     availability_zone       = "us-east-1b"
     map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "sn3"{
-    cidr_block              = "172.31.3.0/24"
+    cidr_block              = "10.0.3.0/24"
     vpc_id                  = aws_vpc.mainvpc.id
     availability_zone       = "us-east-1c"
     map_public_ip_on_launch = true
